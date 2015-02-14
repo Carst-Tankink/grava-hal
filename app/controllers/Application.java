83dca537492466c79a068bc3e12f6bbc5c1985b7
Carst Tankink <carst.tankink@gmail.com>
2015-02-14 11:48:44 +0100

    Added view/controller for creating games.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,24 @@
 package controllers;
 
-import play.*;
+import models.GravaHal;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
+    }
+
+    public static Result startGravaHal() {
+      GravaHal game = Form.form(GravaHal.class).bindFromRequest().get();
+      game.save();
+      return redirect(routes.Application.game(game.gameId));
+    }
+    
+    public static Result game(String gameId) {
+      return ok();
     }
 
 }
