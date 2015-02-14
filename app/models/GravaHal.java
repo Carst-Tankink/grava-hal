2b19d2d94f9598f025788451096bd14ebfafdc00
Carst Tankink <carst.tankink@gmail.com>
2015-02-14 11:48:02 +0100

    Made game a persistent db entity

--- app/models/GravaHal.java
+++ app/models/GravaHal.java
@@ -1,8 +1,19 @@
 package models;
 
-public class GravaHal {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.db.ebean.Model;
+
+@Entity
+public class GravaHal extends Model {
+  private static final long serialVersionUID = -700730502688708739L;
+  
   private String _playerOne;
   private String _playerTwo;
+  
+  @Id
+  public String gameId;
   
   public GravaHal(String playerOne, String playerTwo) {
     _playerOne = playerOne;
