728ab48b02e5c50736b5e75e2d14da42b6929c0c
Carst Tankink <carst.tankink@gmail.com>
2015-02-16 16:10:54 +0100

    Split Pit test into test exercising superclass and RegularPit class.

--- test/PitTest.java
+++ test/PitTest.java
@@ -7,29 +7,13 @@ import static org.fest.assertions.Assertions.*;
 
 
 public class PitTest {
-
   @Test
-  public void initPitTest() {
-    Pit pit = new RegularPit();
-    assertThat(pit.getContents()).isEqualTo(6);
-  }
-  
-  @Test
-  public void takeTest() {
-    RegularPit pit = new RegularPit();
-    int inHand = pit.takeStones();
-    
-    assertThat(inHand).isEqualTo(6);
-    assertThat(pit.isEmpty());  
-  }
-  
-  @Test
-  public void putTest() {
+  public void pitTest() {
     Pit pit = new RegularPit();
     
-    assertThat(pit.getContents()).isEqualTo(6);
+    int initialContent = pit.getContents();
     pit.putStone();
-    assertThat(pit.getContents()).isEqualTo(7);
+    assertThat(pit.getContents()).isEqualTo(initialContent + 1);
     
   }
 }
---
+++ test/RegularPitTest.java
@@ -0,0 +1,16 @@
+import static org.fest.assertions.Assertions.assertThat;
+import models.RegularPit;
+
+import org.junit.Test;
+
+
+public class RegularPitTest {
+  @Test
+  public void takeTest() {
+    RegularPit pit = new RegularPit();
+    int inHand = pit.takeStones();
+    
+    assertThat(inHand).isEqualTo(6);
+    assertThat(pit.isEmpty());  
+  }
+}
