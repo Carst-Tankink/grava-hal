8d39eca14673aae6ce87c85d874bc1231f7a379d
Carst Tankink <carst.tankink@gmail.com>
2015-02-17 09:56:29 +0100

    Green test for playing a full play, around the board.

--- app/models/GravaHal.java
+++ app/models/GravaHal.java
@@ -49,6 +49,51 @@ public class GravaHal extends Model {
     return board.iterator();
   }
   
+  public void playFrom(String player, int pitIndex) {
+    Side playerSide = getPlayerSide(player);
+    Side currentSide = playerSide;
+    
+    int inHand = playerSide.playFrom(pitIndex);
+    
+    // TODO:  Refactor to method.
+    while (inHand > 0) {
+      currentSide = getNextSide(currentSide);
+      Iterator<RegularPit> pI = currentSide.getPits();
+      
+      while (pI.hasNext() && inHand > 0) {
+        RegularPit pit = pI.next();
+        pit.putStone();
+        inHand--;
+      }
+
+      if (inHand > 0 && currentSide == playerSide) {
+        currentSide.getGravaHalPit().putStone();
+        inHand--;
+      }
+    }
+  }
+  
+  private Side getNextSide(Side side) {
+    int index = board.indexOf(side);
+    return board.get((index + 1) % 2);
+  }
+  
+  private Side getPlayerSide(String player) {
+    // TODO: Better mapping of player -> side
+    int playerIndex = (player == playerOne) ? 0 : 1;
+    Side playerSide = board.get(playerIndex);
+    return playerSide;
+  }
+  
+  public int contentsAt(String player, int pitIndex) {
+    Side side = getPlayerSide(player);
+    return side.getPitContents(pitIndex);
+  }
+  
+  public int contentsAtGravaHal(String player) {
+    return getPlayerSide(player).getGravaHalContents();
+  }
+  
   public GravaHal(String playerOne_, String playerTwo_) {
     playerOne = playerOne_;
     playerTwo = playerTwo_;
--- test/GravaHalTest.java
+++ test/GravaHalTest.java
@@ -5,14 +5,36 @@ import org.junit.*;
 import static org.fest.assertions.Assertions.*;
 
 public class GravaHalTest {
-  /* Initializing a two-player game, with the first player active.
-   */
+  
+  GravaHal game;
+  @Before
+  public void setUp() {
+    game = new GravaHal("Alice", "Bob");
+  }
+  
   @Test
   public void initGame() {
-    GravaHal game = new GravaHal("Alice", "Bob");
     assertThat(game.getPlayerOne()).isEqualTo("Alice");
     assertThat(game.getPlayerTwo()).isEqualTo("Bob");
     
     assertThat(game.getActivePlayer()).isEqualTo("Alice");
+  }
+  
+  @Test
+  public void testPlay() {
+    String playerOne = game.getPlayerOne();
+    String playerTwo = game.getPlayerTwo();
+    game.playFrom(playerOne, 5);
+    
+    assertThat(game.contentsAt(playerOne, 5)).isEqualTo(0);
+    assertThat(game.contentsAtGravaHal(playerOne)).isEqualTo(1);
+    assertThat(game.contentsAt(playerTwo, 0)).isEqualTo(7);
+    assertThat(game.contentsAt(playerTwo, 1)).isEqualTo(7);
+    assertThat(game.contentsAt(playerTwo, 2)).isEqualTo(7);
+    assertThat(game.contentsAt(playerTwo, 3)).isEqualTo(7);
+    assertThat(game.contentsAt(playerTwo, 4)).isEqualTo(7);
+    assertThat(game.contentsAt(playerTwo, 5)).isEqualTo(6);
+    
+    
   }
 }
