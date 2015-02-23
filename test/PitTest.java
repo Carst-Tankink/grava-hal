cd65612c3e6e550108d6d42f5fb1d00969ec0247
Carst Tankink <carst.tankink@gmail.com>
2015-02-23 10:30:37 +0100

    Accidentally had a non-compiling test

--- test/PitTest.java
+++ test/PitTest.java
@@ -9,7 +9,7 @@ import static org.fest.assertions.Assertions.*;
 public class PitTest {
   @Test
   public void pitTest() {
-    Pit pit = new RegularPit();
+    Pit pit = new RegularPit(3);
     
     int initialContent = pit.getContents();
     pit.putStone();
